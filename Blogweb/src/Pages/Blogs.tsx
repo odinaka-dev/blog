9a4735db4411b9f8151f63e1d20551b8f999826f
uchechukwu <uchechukwupeter22@gmail.com>
2025-03-28 15:42:23 +0100

    responsive loader

--- Blogweb/src/Pages/Blogs.tsx
+++ Blogweb/src/Pages/Blogs.tsx
@@ -129,7 +129,7 @@ const BlogsPosts = () => {
           ))}
         </article>
       ) : (
-        <div className="grid grid-cols-3 gap-8 items-center my-8">
+        <div className="grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-3 gap-8 items-center my-8">
           <Load />
           <Load />
           <Load />
--- Blogweb/src/Pages/Home.tsx
+++ Blogweb/src/Pages/Home.tsx
@@ -228,7 +228,7 @@ const PopularPost = () => {
           ))}
         </article>
       ) : (
-        <div className="grid grid-cols-3 gap-8 items-center my-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 items-center my-4">
           <Load />
           <Load />
           <Load />
@@ -331,7 +331,7 @@ const SecondPopularPosts = () => {
           ))}
         </article>
       ) : (
-        <div className="grid grid-cols-3 gap-8 items-center my-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 items-center my-8">
           <Load />
           <Load />
           <Load />
